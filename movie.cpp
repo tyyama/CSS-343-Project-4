4e0fc6a3df6f5558e8dae5c3e1263c681515edde
tyyama <yamamototyler70@gmail.com>
2016-06-06 23:18:00 -0700

    Changed Movie factory method to accept char as parameter, added some new methods to Movie.

--- movie.cpp
+++ movie.cpp
@@ -1,18 +1,23 @@
-Movie::Movie(int stock, int year, string title, string director) : stock(stock), yearRelease(year), title(title), director(director){
+Movie::Movie(int stock, int year, string title, string director) : stock(stock), yearRelease(year), title(title), director(director){}
 	
 
 //Factory Method
-Movie *Movie::store_movie(int movieType){
-  if (movieType == F) return new ComedyMovie;
-  if (movieType == D) return new DramaMovie;
-  if (movieType == C) return new ClassicMovie;
+Movie *Movie::store_movie(char movieType){
+  if (movieType == 'F') return new ComedyMovie;
+  if (movieType == 'D') return new DramaMovie;
+  if (movieType == 'C') return new ClassicMovie;
 	else return NULL;
 
-//Factory Method
-Movie *Movie::store_movie(string movieType){
-  if (movieType == "F") return new ComedyMovie;
-  if (movieType == "D") return new DramaMovie;
-  if (movieType == "C") return new ClassicMovie;
-	else return NULL;
+}
 
+bool Movie::sameType(const Movie& other) const{
+	return (typeid(*this)==typeid(other));
+}
+
+bool Movie::operator==(const Movie & rhs) const{
+	if(!sameType(rhs)){
+		return false;
+	}else{
+		return (yearRelease == rhs.yearRelease && stock == rhs.stock && title == rhs.title && director == rhs.director);
+	}
 }
